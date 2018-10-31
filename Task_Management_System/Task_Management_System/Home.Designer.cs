14f2bfb531042b665b7b6f823e196d6923094c5c
a.resendeviana683 <a.resendeviana683@mybvc.ca>
2018-10-31 15:22:54 -0600

    add Board from layout and delete board from layout were added

--- Task_Management_System/Task_Management_System/Home.Designer.cs
+++ Task_Management_System/Task_Management_System/Home.Designer.cs
@@ -29,14 +29,21 @@
         private void InitializeComponent()
         {
             this.AddBoard = new System.Windows.Forms.Button();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.Board2 = new System.Windows.Forms.TabPage();
+            this.Board1 = new System.Windows.Forms.TabPage();
             this.BoardArea = new System.Windows.Forms.TabControl();
             this.DeleteBoard = new System.Windows.Forms.Button();
             this.DeleteList = new System.Windows.Forms.Button();
             this.AddList = new System.Windows.Forms.Button();
-            this.tabPage1.SuspendLayout();
+            this.List = new System.Windows.Forms.Panel();
+            this.DeleteTask = new System.Windows.Forms.Button();
+            this.AddTask = new System.Windows.Forms.Button();
+            this.ListArea = new System.Windows.Forms.Panel();
+            this.ListName = new System.Windows.Forms.Label();
+            this.Board1.SuspendLayout();
             this.BoardArea.SuspendLayout();
+            this.List.SuspendLayout();
+            this.ListArea.SuspendLayout();
             this.SuspendLayout();
             // 
             // AddBoard
@@ -50,33 +57,34 @@
             this.AddBoard.UseVisualStyleBackColor = true;
             this.AddBoard.Click += new System.EventHandler(this.AddBoard_Click);
             // 
-            // tabPage2
+            // Board2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 29);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1903, 857);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.Board2.Location = new System.Drawing.Point(4, 29);
+            this.Board2.Name = "Board2";
+            this.Board2.Padding = new System.Windows.Forms.Padding(3);
+            this.Board2.Size = new System.Drawing.Size(1903, 857);
+            this.Board2.TabIndex = 1;
+            this.Board2.Text = "Board2";
+            this.Board2.UseVisualStyleBackColor = true;
             // 
-            // tabPage1
+            // Board1
             // 
-            this.tabPage1.Controls.Add(this.DeleteList);
-            this.tabPage1.Controls.Add(this.AddList);
-            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tabPage1.Location = new System.Drawing.Point(4, 29);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1903, 857);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.Board1.Controls.Add(this.ListArea);
+            this.Board1.Controls.Add(this.DeleteBoard);
+            this.Board1.Controls.Add(this.AddList);
+            this.Board1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Board1.Location = new System.Drawing.Point(4, 29);
+            this.Board1.Name = "Board1";
+            this.Board1.Padding = new System.Windows.Forms.Padding(3);
+            this.Board1.Size = new System.Drawing.Size(1903, 857);
+            this.Board1.TabIndex = 0;
+            this.Board1.Text = "Board1";
+            this.Board1.UseVisualStyleBackColor = true;
             // 
             // BoardArea
             // 
-            this.BoardArea.Controls.Add(this.tabPage1);
-            this.BoardArea.Controls.Add(this.tabPage2);
+            this.BoardArea.Controls.Add(this.Board1);
+            this.BoardArea.Controls.Add(this.Board2);
             this.BoardArea.Location = new System.Drawing.Point(13, 68);
             this.BoardArea.Name = "BoardArea";
             this.BoardArea.SelectedIndex = 0;
@@ -86,19 +94,20 @@
             // DeleteBoard
             // 
             this.DeleteBoard.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DeleteBoard.Location = new System.Drawing.Point(180, 14);
+            this.DeleteBoard.Location = new System.Drawing.Point(1736, 6);
             this.DeleteBoard.Name = "DeleteBoard";
             this.DeleteBoard.Size = new System.Drawing.Size(161, 48);
             this.DeleteBoard.TabIndex = 2;
             this.DeleteBoard.Text = "- Board";
             this.DeleteBoard.UseVisualStyleBackColor = true;
+            this.DeleteBoard.Click += new System.EventHandler(this.DeleteBoard_Click);
             // 
             // DeleteList
             // 
             this.DeleteList.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DeleteList.Location = new System.Drawing.Point(173, 7);
+            this.DeleteList.Location = new System.Drawing.Point(3, 697);
             this.DeleteList.Name = "DeleteList";
-            this.DeleteList.Size = new System.Drawing.Size(161, 48);
+            this.DeleteList.Size = new System.Drawing.Size(321, 48);
             this.DeleteList.TabIndex = 4;
             this.DeleteList.Text = "- List";
             this.DeleteList.UseVisualStyleBackColor = true;
@@ -113,18 +122,70 @@
             this.AddList.Text = "+ List";
             this.AddList.UseVisualStyleBackColor = true;
             // 
+            // List
+            // 
+            this.List.Controls.Add(this.ListName);
+            this.List.Controls.Add(this.DeleteList);
+            this.List.Controls.Add(this.AddTask);
+            this.List.Location = new System.Drawing.Point(3, 3);
+            this.List.Name = "List";
+            this.List.Size = new System.Drawing.Size(327, 748);
+            this.List.TabIndex = 5;
+            // 
+            // DeleteTask
+            // 
+            this.DeleteTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteTask.Location = new System.Drawing.Point(618, 16);
+            this.DeleteTask.Name = "DeleteTask";
+            this.DeleteTask.Size = new System.Drawing.Size(120, 46);
+            this.DeleteTask.TabIndex = 7;
+            this.DeleteTask.Text = "- Task";
+            this.DeleteTask.UseVisualStyleBackColor = true;
+            // 
+            // AddTask
+            // 
+            this.AddTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddTask.Location = new System.Drawing.Point(3, 51);
+            this.AddTask.Name = "AddTask";
+            this.AddTask.Size = new System.Drawing.Size(321, 46);
+            this.AddTask.TabIndex = 6;
+            this.AddTask.Text = "+ Task";
+            this.AddTask.UseVisualStyleBackColor = true;
+            // 
+            // ListArea
+            // 
+            this.ListArea.AutoScroll = true;
+            this.ListArea.Controls.Add(this.List);
+            this.ListArea.Location = new System.Drawing.Point(7, 61);
+            this.ListArea.Name = "ListArea";
+            this.ListArea.Size = new System.Drawing.Size(1890, 790);
+            this.ListArea.TabIndex = 6;
+            // 
+            // ListName
+            // 
+            this.ListName.AutoSize = true;
+            this.ListName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ListName.Location = new System.Drawing.Point(96, 11);
+            this.ListName.Name = "ListName";
+            this.ListName.Size = new System.Drawing.Size(142, 32);
+            this.ListName.TabIndex = 7;
+            this.ListName.Text = "List Name";
+            // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1936, 970);
-            this.Controls.Add(this.DeleteBoard);
             this.Controls.Add(this.BoardArea);
+            this.Controls.Add(this.DeleteTask);
             this.Controls.Add(this.AddBoard);
             this.Name = "Home";
             this.Text = "Home";
-            this.tabPage1.ResumeLayout(false);
+            this.Board1.ResumeLayout(false);
             this.BoardArea.ResumeLayout(false);
+            this.List.ResumeLayout(false);
+            this.List.PerformLayout();
+            this.ListArea.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -132,11 +193,16 @@
         #endregion
 
         private System.Windows.Forms.Button AddBoard;
-        private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage Board2;
+        private System.Windows.Forms.TabPage Board1;
         private System.Windows.Forms.TabControl BoardArea;
         private System.Windows.Forms.Button DeleteBoard;
         private System.Windows.Forms.Button DeleteList;
         private System.Windows.Forms.Button AddList;
+        private System.Windows.Forms.Panel List;
+        private System.Windows.Forms.Button AddTask;
+        private System.Windows.Forms.Button DeleteTask;
+        private System.Windows.Forms.Panel ListArea;
+        private System.Windows.Forms.Label ListName;
     }
 }
--- Task_Management_System/Task_Management_System/Home.cs
+++ Task_Management_System/Task_Management_System/Home.cs
@@ -19,7 +19,16 @@ namespace Task_Management_System
 
         private void AddBoard_Click(object sender, EventArgs e)
         {
+            TabPage aBoard = new TabPage("Board " + (BoardArea.TabPages.Count + 1));
+            BoardArea.TabPages.Add(aBoard);
+        }
 
+        private void DeleteBoard_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Do you really want to delete the current board? \rThis action is not reversible.", "DELETE BOARD!" , MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                BoardArea.TabPages.Remove(BoardArea.SelectedTab);
+            }
         }
     }
 }
